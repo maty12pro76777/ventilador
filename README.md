13e2ce50bfbb9e6122e11016ef0b077596b5088d
maty12pro76777 <matylolis@outlook.com>
2024-05-26 19:27:32 -0600

    Archivos iniciales para el proyecto MakeCode

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Abra esta pagina en [https://maty12pro76777.github.io/ventilador/](https://maty12pro76777.github.io/ventilador/)
+
+## Usar como extensión
+
+Este repositorio puede ser añadido como una **extensión** en MakeCode.
+
+* abra [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* haga clic en **New Project**
+* haga clic en **Extensiones** en el menú del engranaje
+* buscar **https://github.com/maty12pro76777/ventilador** e importar
+
+## Edita este proyecto
+
+Para editar este repositorio en MakeCode.
+
+* abra [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* haga clic en **Import** luego haga clic en **Import URL**
+* pegue **https://github.com/maty12pro76777/ventilador** y haga clic en importar
+
+#### Metadatos (utilizados para búsqueda, renderizado)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,45 @@
+input.onButtonPressed(Button.A, function () {
+    hummingbird.setLED(ThreePort.Two, 100)
+    for (let index = 0; index < 4; index++) {
+        hummingbird.setPositionServo(FourPort.One, 0)
+        hummingbird.setRotationServo(FourPort.One, 50)
+        hummingbird.setRotationServo(FourPort.One, 50)
+        hummingbird.setRotationServo(FourPort.One, 50)
+        hummingbird.setRotationServo(FourPort.One, 50)
+        hummingbird.setRotationServo(FourPort.One, 50)
+        hummingbird.setRotationServo(FourPort.One, 50)
+        hummingbird.setRotationServo(FourPort.One, 50)
+        hummingbird.setRotationServo(FourPort.One, 50)
+        hummingbird.setRotationServo(FourPort.One, 50)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    hummingbird.setLED(ThreePort.One, 100)
+    for (let index = 0; index < 4; index++) {
+        hummingbird.setPositionServo(FourPort.One, 0)
+        hummingbird.setRotationServo(FourPort.One, 10)
+        hummingbird.setRotationServo(FourPort.One, 10)
+        hummingbird.setRotationServo(FourPort.One, 10)
+        hummingbird.setRotationServo(FourPort.One, 10)
+        hummingbird.setRotationServo(FourPort.One, 10)
+        hummingbird.setRotationServo(FourPort.One, 10)
+        hummingbird.setRotationServo(FourPort.One, 10)
+        hummingbird.setRotationServo(FourPort.One, 10)
+        hummingbird.setRotationServo(FourPort.One, 10)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    hummingbird.setLED(ThreePort.Three, 100)
+    for (let index = 0; index < 4; index++) {
+        hummingbird.setPositionServo(FourPort.One, 0)
+        hummingbird.setRotationServo(FourPort.One, 100)
+        hummingbird.setRotationServo(FourPort.One, 100)
+        hummingbird.setRotationServo(FourPort.One, 100)
+        hummingbird.setRotationServo(FourPort.One, 100)
+        hummingbird.setRotationServo(FourPort.One, 100)
+        hummingbird.setRotationServo(FourPort.One, 100)
+        hummingbird.setRotationServo(FourPort.One, 100)
+        hummingbird.setRotationServo(FourPort.One, 100)
+        hummingbird.setRotationServo(FourPort.One, 100)
+    }
+})
